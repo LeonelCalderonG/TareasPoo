efe3f6b15c885d7102967686bb75617cbfb29d02
LeonelCalderonG <47792402+LeonelCalderonG@users.noreply.github.com>
2019-02-27 23:11:06 -0800

    Dado

---
+++ Dado/Dado/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dado
+{
+    class Dado
+    {
+        private int valor;
+        public string color;
+
+        public Dado(int valor, string co)
+        {
+            this.valor = valor;
+            color = co;
+        }
+
+        public Dado()
+        {
+            valor = 0;
+            color = "";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Random aleatorio = new Random();
+            aleatorio = aleatorio.Next(1, 6);
+            Dado uno = new Dado(aleatorio, color);
+            Dado dos = new Dado(aleatorio, color);
+            Dado tres = new Dado(aleatorio, color);
+        }
+    }
+}
+
