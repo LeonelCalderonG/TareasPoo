8f2e067b690a85f62915b4deaf6492d05faa9534
LeonelCalderonG <47792402+LeonelCalderonG@users.noreply.github.com>
2019-04-05 22:03:43 -0700

    Nueva Tarea

---
+++ Implementacion de Interfaces (Tarea 4)/Implementacion de Interfaces (Tarea 4)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Implementacion_de_Interfaces__Tarea_4_
+{
+    interface IMusico
+    {
+        void Nombre();
+    }
+
+    class Bajista : IMusico
+    {
+       public void Nombre()
+        {
+            Console.WriteLine("Hola, Yo uso el Bajo");
+        }
+    }
+
+    class Guitarrista : IMusico
+    {
+        public void Nombre()
+        {
+            Console.WriteLine("Hola, Yo uso la Guitarra");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<String, IMusico> music = new Dictionary<string, IMusico>();
+            music.Add("Flea", new Bajista());
+            music.Add("Santana", new Guitarrista());
+            IMusico contenido;
+            music.TryGetValue("Flea", out contenido);
+            Console.WriteLine(contenido);
+            Console.ReadKey();
+        }
+    }
+}
